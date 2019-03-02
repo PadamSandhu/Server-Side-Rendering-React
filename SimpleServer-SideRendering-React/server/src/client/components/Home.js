72bfa401cd37abdd90b30b45bec721149f0016c3
Padam <sandhp7@gmail.com>
2019-03-01 20:51:03 -0500

    Added client webpack

--- SimpleServer-SideRendering-React/server/src/client/components/Home.js
+++ SimpleServer-SideRendering-React/server/src/client/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Home = () => {
-    return <div > I am Home Component </div>;
+    return <div > 
+            <h1>I am Home Component!! </h1>
+            <button onClick={()=> console.log("Hello There")}>Press Me</button>
+        </div>;
 };
 
 export default Home
